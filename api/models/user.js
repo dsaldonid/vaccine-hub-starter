1eebd19aab1d6bcfe0d574fae278dfb895815d1c
Diego Saldonid <dsaldoni@ucsd.edu>
2021-06-22 10:57:19 -0700

    vaccone appointment skeleton set up

---
+++ api/models/user.js
@@ -0,0 +1,31 @@
+const { UnauthorizedError } = require("../utils/errors")
+
+class User{
+    static async login(credentials){
+        //submit email and pass
+        //if any fields missing throw error
+        //
+        // lookup the user in the db by email 
+        // iuf the user is found, comapre the submitted password 
+        //with the password in the db 
+        // if there is a match, return the user
+        // if any of this goes wrong throw an error
+        throw new UnauthorizedError("Invalid emal/password combo")
+    }
+
+    static async register(credentials){
+        //user submits email pw appoint status and number of appoint
+        // if any are missing throw erro
+        //
+        // make sure not user already exists in the system withthat email 
+        //if one does, throw an error
+        //
+        //take the users password, and hash it
+        //take the users email and lowercase it
+        //
+        // create a new in the db with their info
+        //return the user
+    }
+}
+
+module.exports = User
---
+++ api/routes/auth.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.router()
+
+router.post("/login", async (req,res,next) => {
+    try{
+        //take user email and pass and attempt to auth them 
+    } catch (err){
+        next(err)
+    }
+})
+
+router.post("/register", async (req,res,next) => {
+    try{
+        //take user email password, appoint status and number of appoints
+        //and create a new user in our database
+    } catch(err){
+        next(err)
+    }
+})
+
+module.exports = router
